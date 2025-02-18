87e47f63481ee5f5cdc994e2b1d3a42559fc2d14
theo.delporte <theo.delporte@ynov.com>
2025-02-18 09:19:56 +0100

    début page sur symfony

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class InscriptionController extends AbstractController
+class InscriptionController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
-    public function index(): Response
-    {
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $utilisateur = new Utilisateur();
+
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $hashedPassword = $passwordHasher->hashPassword(
+                $utilisateur,
+                $utilisateur->getMotDePasse()
+            );
+            $utilisateur->setMotDePasse($hashedPassword);
+
+            $photoFile = $form->get('profilePicture')->getData();
+            if ($photoFile) {
+                $nomFichier = uniqid() . '.' . $photoFile->guessExtension();
+                $photoFile->move(
+                    $this->getParameter('photo_directory'),
+                    $nomFichier
+                );
+                $utilisateur->setProfilePicture($nomFichier);
+            }
+
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Inscription réussie !');
+
+            return $this->redirectToRoute('app_inscription');
+        }
+
         return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
+            'form' => $form->createView(),
         ]);
     }
 }
